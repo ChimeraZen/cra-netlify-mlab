c6920258b7079b7845f1933bf320a1ca98dd5fb8
Elijah Liedtke <chimera.zen@gmail.com>
2018-12-31 01:20:01 -0500

    Minor updates

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # cra-netlify-mlab
 
 See the full guide [How to make a CRUD with Netlify, create-react-app & mLab MongoDB](https://medium.com/@chimera.zen/how-to-make-a-crud-with-netlify-create-react-app-mlab-mongodb-927a2e7ad2c3) for more information.
+Demo: [https://cra-netlify-mlab.netlify.com/](https://cra-netlify-mlab.netlify.com/)
--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h1>Create-react-app, Netlify & mLab Demo</h1>
+        <p>See the full guide <a href='https://medium.com/@chimera.zen/how-to-make-a-crud-with-netlify-create-react-app-mlab-mongodb-927a2e7ad2c3'>How to make a CRUD with Netlify, create-react-app & mLab MongoDB</a> for more information.<br />
+GitHub repository: <a href='https://github.com/ChimeraZen/cra-netlify-mlab'>https://github.com/ChimeraZen/cra-netlify-mlab</a></p>
         <ProductTable />
       </div>
     )
